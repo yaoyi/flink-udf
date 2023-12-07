7562155546cc4822996612d6bc89f6ca0b726e7b
yaoyi <linyaoyi@me.com>
2023-12-07 19:00:16 +0800

    save

--- src/main/java/com/qmt/flink/udf/ToTimeZone.java
+++ src/main/java/com/qmt/flink/udf/ToTimeZone.java
@@ -28,7 +28,7 @@ public class ToTimeZone extends ScalarFunction {
     MARKETPLACE_ID_TO_TIMEZONE.put("A19VAU5U5O7RUS", "Asia/Singapore");
   }
   public String eval(String marketplaceId) {
-    timezone = MARKETPLACE_ID_TO_TIMEZONE.get(marketplaceId)
+    timezone = MARKETPLACE_ID_TO_TIMEZONE.get(marketplaceId);
     return timezone == null ? "UTC" : timezone;
   }
 }
